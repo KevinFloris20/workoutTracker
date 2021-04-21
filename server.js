5ceac1ca852339fd521999918a49aa9dedd2fd20
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-04-21 13:20:48 -0400

    server setup

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3029;
 
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Tracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
